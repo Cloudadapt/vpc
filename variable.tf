65291ba2e68b7f26b4bb9fa20d0ffe472077689d
Grace Ogunniyi <ariyike.ogunniyi@gmail.com>
2023-06-14 14:26:38 -0500

    vpc infrastructure modules added

---
+++ variable.tf
@@ -0,0 +1,42 @@
+# vpc variables
+variable "vpc_cidr" {
+    default     = "10.0.0.0/16"
+    description = "vpc cidr block"
+    type        =  string
+}
+
+variable "public_subnet_az1_cidr" {
+    default     = "10.0.101.0/24"
+    description = "public subnet az1 cidr"
+    type        =  string
+}
+
+variable "public_subnet_az2_cidr" {
+    default     = "10.0.102.0/24"
+    description = "public subnet az2 cidr"
+    type        =  string
+}
+
+variable "private_app_subnet_az1_cidr" {
+    default     = "10.0.201.0/24"
+    description = "private app subnet az1 cidr"
+    type        =  string
+}
+
+variable "private_app_subnet_az2_cidr" {
+    default     = "10.0.202.0/24"
+    description = "private app subnet az2 cidr"
+    type        =  string
+}
+
+variable "private_data_subnet_az1_cidr" {
+    default     = "10.0.203.0/24"
+    description = "private data subnet az1 cidr"
+    type        =  string
+}
+
+variable "private_data_subnet_az2_cidr" {
+    default     = "10.0.204.0/24"
+    description = "private data subnet az2 cidr"
+    type        =  string
+}
